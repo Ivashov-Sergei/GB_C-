3c05303ba038dfede9f91ddd22110fea1ae88898
Ivashov_Sergei <spaunok665@mail.ru>
2022-08-27 10:17:00 +0300

    add ex34 36 38 41 43

---
+++ ex41/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m)
+{
+    int i = 0;
+    while (i < m)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+    }
+}
+
+int Comparison(int[] massiveNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < massiveNumbers.Length; i++)
+    {
+        if (massiveNumbers[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
